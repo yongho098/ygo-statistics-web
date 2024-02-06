b57e32609393df1b4eaa192c8d91ff2a811dc7f1
yongho098 <70485221+yongho098@users.noreply.github.com>
2024-02-06 14:52:19 -0500

    changed debug value

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r&2x-2*&h7rjxx*6qur&=q^6jt#n1x7b2ii^t!hqqyr7ev80=%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'yugioh-statistics.azurewebsites.net']
 
